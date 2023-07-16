4f62be71e3d3fb7767aa639427214a90856a4adc
Kristopher Ong <k.ong@arcanys.com>
2023-07-16 16:14:52 +0800

    🐞 fix: Issue when there is empty goal entered

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -4,26 +4,41 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = (props: { onAddGoal: (enteredValue: string) => void }) => {
-    const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
-    const goalInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setEnteredValue(event.target.value);
-    };
+  const goalInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
 
-    const formSubmitHandler = (event: SyntheticEvent) => {
-        event.preventDefault();
-        props.onAddGoal(enteredValue);
-    };
+  const formSubmitHandler = (event: SyntheticEvent) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
 
-    return (
-        <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <label>Course Goal</label>
-                <input type="text" onChange={goalInputChangeHandler} />
-            </div>
-            <Button type="submit">Add Goal</Button>
-        </form>
-    );
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? 'red' : '#ccc',
+            background: !isValid ? 'salmon' : 'transparent',
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
 };
 
 export default CourseInput;
